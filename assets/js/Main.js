0c9e7a523fc314a4e37449fb72df94c7b9ff245e
Eridanus_Sora <haolinaihui@Gmail.com>
2016-08-19 17:05:19 +0800

    增加一些注释 高等级事件高亮

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -8,21 +8,55 @@ $(document).ready(()=>{
     ready: function () {
       // 连接Websocket
       var socket = io('192.168.71.128:3737');
+      var levelChart = {
+        1: '一般事件',
+        2: '有趣的事件',
+        3: '重要事件',
+        4: '紧急事件',
+        5: '世界毁灭'
+      };
+      var showNotification;
+      Notification.requestPermission((status) => {
+        if (status === "granted"){
+          /**
+           * 显示通知
+           * {string} title 通知标题, {string} content 通知内容, {string} link 通知链接
+           */
+          showNotification = function (title, content, link) {
+            var notice = new Notification(title, {
+              body: content
+            });
+            notice.onclick = () => {
+              window.open(link);
+            }
+          }
+        }
+      });
+
       socket.on('event', (data) => {
         // 尝试按JSON解析
         try{
           var event = JSON.parse(data);
-          console.log(event);
           var item = {};
           ["title", "content", "link"].forEach(key =>{
             item[key] = event && event.data && event.data[key] || undefined;
           });
-          console.log(item);
+          ["hash", "spiderName"].forEach(key => {
+            item[key] = event && event[key]
+          });
+
+          item.level = levelChart[event && event.level];
+          if (event.level >= 3) item.title = '<span style="color: red">' + item.title + '</span>';
+
           if (item.title && item.content && item.link){
-            this.events.push(item);
+            this.events.unshift(item);
+            if (event.level && event.level >= 3){
+              showNotification(item.title, item.content, item.link)
+            }
           }
         }
         catch(e){
+          console.log(e);
           console.log('无法解析事件');
         }
       });
