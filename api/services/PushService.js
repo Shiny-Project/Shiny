d92408fcb30c6b0c91037cbeca48636bffea82ab
Eridanus Sora <haolinaihui@Gmail.com>
2018-07-18 23:04:52 +0800

    improve push service

--- api/services/PushService.js
+++ api/services/PushService.js
@@ -5,123 +5,12 @@ module.exports = {
    * @param body
    */
   sendSocket: function (type, body) {
-    let io = require('socket.io-client');
-    let socket = io.connect('http://websocket.shiny.kotori.moe:3737', {
-      reconnect: true
-    });
-    socket.on('connect', function () {
-      socket.emit('event', JSON.stringify(body));
-      socket.close();
-    });
+    const pusher = require('./Pusher/Websocket');
+    pusher.sendSocket(type, body);
   },
-  /**
-   * 发送微博
-   * @param text
-   * @param id
-   * @param pic
-   */
-  sendWeibo: async function (text, id = 0, pic = undefined) {
-    // 微博字数
-    const getLength = (str) => {
-      const unicodeCharacters = str.match(/[^\x00-\x80]/g) || [];
-      const otherCharacters = str.replace(/[^\x00-\x80]/g, "");
-      return unicodeCharacters.length + Math.floor(otherCharacters.length / 2);
-    };
-
-    // 分隔超长微博
-    const splitByLength = (str, length = 100) => {
-      const result = [];
-      let counter = 0;
-      let tempStr = "";
-      for (const i of str) {
-        counter += getLength(i);
-        tempStr += i;
-        if (counter >= length) {
-          result.push(`${tempStr} https://shiny.kotori.moe/`);
-          tempStr = "";
-          counter = 0;
-        }
-      }
-      if (tempStr.length > 0) {
-        result.push(`${tempStr} https://shiny.kotori.moe/`);
-      }
-      if (result.length > 1) {
-        // 多条时显示发送序号
-        return result.map((v, i) => `(${i + 1}/${result.length}) ${v}`);
-      } else {
-        return result;
-      }
-    };
-
-    const accessKey = sails.config.common.weibo_access_key;
-    const request = require('request-promise');
-    const fs = require('fs');
-    if (!pic) {
-      for (const i of splitByLength(text)) {
-        try {
-          const result = await request.post({
-            url: 'https://api.weibo.com/2/statuses/share.json', form: {
-              access_token: accessKey,
-              status: i
-            }
-          });
-          Job.create({
-            type: 'push',
-            info: JSON.stringify({
-              channel: 'weibo',
-              text: i,
-              response: result
-            }),
-            status: 'success'
-          }).then(() => {
-            // do nothing
-          });
-        }
-        catch (e) {
-          console.log(e);
-          // Whatever..
-        }
-        await CommonUtils.sleep(1000);
-      }
-    } else {
-      for (const i of splitByLength(text)) {
-        try {
-          await request.post({
-            url: 'https://api.weibo.com/2/statuses/share.json', formData: {
-              access_token: accessKey,
-              status: i,
-              pic: fs.createReadStream(pic)
-            }
-          });
-        }
-        catch (e) {
-          console.log(e);
-        }
-        await CommonUtils.sleep(1000);
-      }
-      fs.unlinkSync(pic);
-    }
-  },
-  sendTeleGram: function (text) {
-    let request = require('request');
-    let botAPIKey = sails.config.common.telegramBotAPIKey;
-    let channel = sails.config.common.telegramChannel;
-    try {
-      request.post({
-        url: `https://api.telegram.org/bot${botAPIKey}/sendMessage`,
-        form: {
-          chat_id: `@${channel}`,
-          text: text
-        }
-      }, function (error, response, body) {
-        if (error) {
-          console.log(response);
-        }
-      });
-    }
-    catch (e) {
-      console.log(e);
-    }
+  sendTelegram: function (text) {
+    const pusher = require('./Pusher/Telegram');
+    pusher.sendTeleGram(text);
   },
   /**
    * 推送到社交平台
@@ -157,8 +46,9 @@ module.exports = {
         return;
       }
     }
+    const WeiboPusher = require('./Pusher/Weibo');
     for (const result of parseResults) {
-      this.sendWeibo(result.text, id, result.pic).then(() => {
+      WeiboPusher.sendWeibo(result.text, id, result.pic).then(() => {
         //
       });
     }
---
+++ api/services/Pusher/Weibo.js
@@ -0,0 +1,166 @@
+module.exports = {
+  /**
+   * 发送微博
+   * @param text
+   * @param id
+   * @param pic
+   */
+  sendWeibo: async function (text, id = 0, pic = undefined) {
+    // 微博字数
+    const getLength = (str) => {
+      const unicodeCharacters = str.match(/[^\x00-\x80]/g) || [];
+      const otherCharacters = str.replace(/[^\x00-\x80]/g, "");
+      return unicodeCharacters.length + Math.floor(otherCharacters.length / 2);
+    };
+
+    // 分隔超长微博
+    const splitByLength = (str, length = 100) => {
+      const result = [];
+      let counter = 0;
+      let tempStr = "";
+      for (const i of str) {
+        counter += getLength(i);
+        tempStr += i;
+        if (counter >= length) {
+          result.push(`${tempStr} https://shiny.kotori.moe/`);
+          tempStr = "";
+          counter = 0;
+        }
+      }
+      if (tempStr.length > 0) {
+        result.push(`${tempStr} https://shiny.kotori.moe/`);
+      }
+      if (result.length > 1) {
+        // 多条时显示发送序号
+        return result.map((v, i) => `(${i + 1}/${result.length}) ${v}`);
+      } else {
+        return result;
+      }
+    };
+
+    const accessKey = sails.config.common.weibo_access_key;
+    const request = require('request-promise');
+    const fs = require('fs');
+
+    let retries = 3;
+    let errorFlag, errorText, response;
+
+    if (!pic) {
+      // 不带图
+      // 分隔超长微博
+      for (const i of splitByLength(text)) {
+        retries = 3;
+        // 记录任务
+        const job = await Job.create({
+          type: 'push',
+          info: JSON.stringify({
+            channel: 'weibo',
+            text: i
+          }),
+          status: 'processing'
+        });
+        while (retries > 0) {
+          try {
+            const result = await request.post({
+              url: 'https://api.weibo.com/2/statuses/share.json', form: {
+                access_token: accessKey,
+                status: i
+              }
+            });
+            retries = 0;
+            errorFlag = false;
+            response = result;
+          }
+          catch (e) {
+            errorFlag = true;
+            errorText = e;
+            retries--;
+            await CommonUtils.sleep(3000); // 冷却三秒
+          }
+        }
+        if (errorFlag) {
+          // 记录错误
+          await Job.update({
+            id: job.id
+          }).set({
+            status: 'failed',
+            info: JSON.stringify({
+              ...JSON.parse(job.info),
+              error: JSON.stringify(errorText)
+            })
+          });
+        } else {
+          // 记录返回结果
+          await Job.update({
+            id: job.id
+          }).set({
+            status: 'success',
+            info: JSON.stringify({
+              ...JSON.parse(job.info),
+              response
+            })
+          });
+        }
+      }
+
+
+    } else {
+      // 带图
+      // 记录任务
+      const job = await Job.create({
+        type: 'push',
+        info: JSON.stringify({
+          channel: 'weibo',
+          text: text
+        }),
+        status: 'processing'
+      });
+
+      retries = 3;
+      while (retries > 0) {
+        try {
+          const result = await request.post({
+            url: 'https://api.weibo.com/2/statuses/share.json', formData: {
+              access_token: accessKey,
+              status: text,
+              pic: fs.createReadStream(pic)
+            }
+          });
+          retries = 0;
+          errorFlag = false;
+          response = result;
+        }
+        catch (e) {
+          errorFlag = true;
+          errorText = e;
+          retries--;
+          await CommonUtils.sleep(3000);
+        }
+      }
+
+      if (errorFlag) {
+        await Job.update({
+          id: job.id
+        }).set({
+          status: 'failed',
+          info: JSON.stringify({
+            ...JSON.parse(job.info),
+            error: JSON.stringify(errorText)
+          })
+        });
+      } else {
+        await Job.update({
+          id: job.id
+        }).set({
+          status: 'success',
+          info: JSON.stringify({
+            ...JSON.parse(job.info),
+            response: response
+          })
+        });
+      }
+
+      fs.unlinkSync(pic);
+    }
+  },
+};
