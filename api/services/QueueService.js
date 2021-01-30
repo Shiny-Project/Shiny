4a8597c3cec89ad47516ca02dc9c4b38e5417607
Eridanus Sora <sora@sound.moe>
2021-01-30 21:46:57 +0800

    queue service

--- api/services/QueueService.js
+++ api/services/QueueService.js
@@ -1,6 +1,24 @@
+const { SendMessageCommand, SQSClient } = require("@aws-sdk/client-sqs");
 class QueueService {
     constructor() {
-        console.log(sails);
+        this.client = new SQSClient({
+            credentials: {
+                accessKeyId: sails.config.common.AWS_ACCESS_KEY_ID,
+                secretAccessKey: sails.config.common.AWS_SECRET_ACCESS_KEY,
+            },
+            region: sails.config.common.AWS_REGION,
+            maxAttempts: 3,
+        });
+        this.queueUrl = sails.config.common.AWS_SQS_URL;
+    }
+
+    async sendMessage(payload) {
+        await this.client.send(
+            new SendMessageCommand({
+                QueueUrl: this.queueUrl,
+                MessageBody: JSON.stringify(payload),
+            })
+        );
     }
 }
 
