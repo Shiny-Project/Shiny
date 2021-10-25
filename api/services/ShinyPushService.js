ef3cd84ed021f08e413a35dbbd721e96c16f5ec4
Eridanus Sora <sora@sound.moe>
2021-10-25 23:13:58 +0800

    fix bugs

--- api/services/ShinyPushService.js
+++ api/services/ShinyPushService.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 class ShinyPushService {
     async push(payload = {}) {
-        const { channels = [], text, images = [], account, eventId } = payload;
+        const { channels = [], text, images = [], account, eventId, title, level, link } = payload;
         const createdJobs = (
             await axios.post("http://push.shiny.kotori.moe/push/send", {
                 channels,
@@ -9,6 +9,9 @@ class ShinyPushService {
                 images,
                 account,
                 eventId,
+                title,
+                level,
+                link,
             })
         ).data;
         return createdJobs;
