f0f4a7476091c0bd6e512fe14407ce985691d694
Eridanus Sora <sora@sound.moe>
2023-07-23 16:09:06 +0800

    update deps

--- api/services/DataPostProcessService.js
+++ api/services/DataPostProcessService.js
@@ -3,20 +3,26 @@ class DataPostProcessService {
         const { publisher, data } = event;
         const parsedData = JSON.parse(data);
         if (publisher === "JMATyphoon") {
-            const { typhoon_data } = parsedData || {};
-            const { current } = typhoon_data || {};
-            const { number } = current || {};
-            if (number) {
-                try {
-                    await DataTimeSeries.create({
-                        key: `jma_typhoon_${number}`,
-                        data: JSON.stringify(typhoon_data),
-                    });
-                } catch (e) {
-                    Sentry.captureException(e);
-                }
+            await this.recordJMATyphoon(parsedData);
+        }
+    }
+    async recordJMATyphoon(parsedData) {
+        const { typhoon_data } = parsedData || {};
+        const { current } = typhoon_data || {};
+        const { number } = current || {};
+        if (number) {
+            try {
+                await DataTimeSeries.create({
+                    key: `jma_typhoon_${number}`,
+                    data: JSON.stringify(typhoon_data),
+                });
+            } catch (e) {
+                Sentry.captureException(e);
             }
         }
+    }
+    async recordAlert(parsedData) {
+        
     }
 }
 
